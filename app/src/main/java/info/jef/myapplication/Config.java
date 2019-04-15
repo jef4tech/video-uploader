fc667a507452d70f0eae154323112de95352b0b5
jef4tech <jef4tech@gmail.com>
2019-04-15 11:19:20 +0530

    Initial commit

---
+++ app/src/main/java/info/jef/myapplication/Config.java
@@ -0,0 +1,11 @@
+package info.jef.myapplication;
+
+/**
+ * Created by Belal on 11/23/2015.
+ */
+public class Config {
+    public static final String UPLOAD_URL= "https://jeftik.000webhostapp.com/test/vid.php";
+
+    public static final String TAG_URL = "url";
+    public static final String TAG_SIZE = "size";
+}
